ce8a775d2cb49ff406dc0c75cbd5d110207e00ef
Pritom Sarker <pritoms@gmail.com>
2022-09-19 17:18:29 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='npwc',
+    version='0.0.1',
+    packages=find_packages(),
+    url='https://github.com/pritoms/Neural-Programmer-White-box-Coder',
+    license='MIT',
+    author='Pritom Sarker',
+    author_email='pritomsarker@gmail.com',
+    description='Neural Programmer-White-box-Coder (NPWC) is a research project that focuses on bridging the gap between declarative deep learning and symbolic AI by introducing a new architecture that combines the benefits of both approaches.'
+)
